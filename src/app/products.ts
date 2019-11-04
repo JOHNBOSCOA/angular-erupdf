02f55ae6af6bf923f2ee5c4c91ccb8a714e01fb2
JOHNBOSCOA <johnboscoadam@gmail.com>
2019-11-04 17:08:39 +0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    distributor:'Zebida'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    distributor:'Masanja'
+  },
+  {
+    name: 'Phone Standard',
+    price: 701,
+    description: 'Its like touch',
+    distributor:'johnbosco'
+  },
+   {
+    name: 'Samsung Galax A7 +',
+    price: 250,
+    description: 'From samsung vendors',
+    distributor:'Hassan'
+  }
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
